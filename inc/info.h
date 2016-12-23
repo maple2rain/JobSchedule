cf0cfe08680b75d1d2a1dc6a205f62030f022017
maple2winter <740858367@qq.com>
2016-12-23 21:16:36 +0800

    add info.h to record info about sql operation

---
+++ inc/info.h
@@ -0,0 +1,30 @@
+﻿#ifndef INFO_H
+#define INFO_H
+
+#include <string>
+
+/* to record the info of sql operation */
+class Info
+{
+private:
+    Info operator=(Info&);
+public:
+    Info() {
+        status = false;
+        info = "";
+    }
+    ~Info() {}
+
+    /* setter */
+    void setStatus(bool _status) { status = _status; }
+    void setInfo(const std::string &_info) { info = _info; }
+
+    /* getter */
+    bool getStatus() { return status; }
+    const std::string &getInfo() { return info; }
+private:
+    bool status;
+    std::string info;
+};
+
+#endif // INFO_H
