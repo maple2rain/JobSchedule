20c0af863af350d94fa0f4b89d2c165af5a9c473
maple2winter <740858367@qq.com>
2016-12-30 14:34:18 +0800

    add icon, gif and so on

---
+++ src/edf.cpp
@@ -0,0 +1,30 @@
+﻿#include "../inc/scheduler.h"
+
+void EDF::schedule_NONE(us16 runtime, JobRecorder &jobRecorder)
+{
+    Scheduler::schedule_NONE(runtime, jobRecorder);
+}
+
+
+void EDF::schedule_PSA(us16 runtime, JobRecorder &jobRecorder)
+{
+
+}
+
+void EDF::schedule_PM(us16 runtime, JobRecorder &jobRecorder)
+{
+
+}
+
+void EDF::schedule_PM_PSA(us16 runtime, JobRecorder &jobRecorder)
+{
+
+}
+
+void EDF::sortJobNone()
+{
+    if(execableJobNum == readyJobNum){
+        readyJobs.sort([](const ptr l, const ptr r){
+                            return l->getDeadLine() < r->getDeadLine(); });
+    }
+}
---
+++ src/sjf.cpp
@@ -0,0 +1,30 @@
+﻿#include "../inc/scheduler.h"
+
+void SJF::schedule_NONE(us16 runtime, JobRecorder &jobRecorder)
+{
+    Scheduler::schedule_NONE(runtime, jobRecorder);
+}
+
+
+void SJF::schedule_PSA(us16 runtime, JobRecorder &jobRecorder)
+{
+
+}
+
+void SJF::schedule_PM(us16 runtime, JobRecorder &jobRecorder)
+{
+
+}
+
+void SJF::schedule_PM_PSA(us16 runtime, JobRecorder &jobRecorder)
+{
+
+}
+
+void SJF::sortJobNone()
+{
+    if(execableJobNum == readyJobNum){
+        readyJobs.sort([](const ptr l, const ptr r){
+                            return l->getLastTime() < r->getLastTime(); });
+    }
+}
